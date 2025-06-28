8f88790d454ab5067e17dd184d5a4c253237d4b1
dharanisree-thangaraj <dharanisree578@gmail.com>
2025-06-28 21:49:47 +0530

    task2.sql

---
+++ Day8/task2.sql
@@ -0,0 +1,13 @@
+INSERT INTO students (name, email, age) VALUES 
+('Alice', 'alice.johnson@example.com', 20),
+('Bob Smith', 'bob.smith@example.com', 22),
+('Charlie Brown', 'charlie.brown@example.com', 21),
+('Diana Prince', 'diana.prince@example.com', 23),
+('Ethan Hunt', 'ethan.hunt@example.com', 24);
+
+INSERT INTO courses (course_name, description) VALUES
+('Java Programming', 'Learn object-oriented programming with Java'),
+('Database Systems', 'Introduction to SQL and relational databases'),
+('Web Development', 'Build modern websites using HTML, CSS, JS'),
+('Data Structures', 'Learn about arrays, stacks, queues, and more'),
+('Operating Systems', 'Understand how OS manages hardware and software');
